7908d18c3db22fac6c8e19de7770174bf0614766
tmax818 <tylermaxwell661@gmail.com>
2022-09-27 16:17:40 -0700

    update solution

--- src/Mammal.java
+++ src/Mammal.java
@@ -3,4 +3,11 @@
  */
 public class Mammal {
 
+    int energyLevel = 100;
+
+    public int displayEnergy(){
+        System.out.println(this.energyLevel);
+        return this.energyLevel;
+    }
+
 }
