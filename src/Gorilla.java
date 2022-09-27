49b776b97b36bc52a6f7e8a20b99d87cd90fef1c
tmax818 <tylermaxwell661@gmail.com>
2022-09-27 13:59:11 -0700

    update main

--- src/Gorilla.java
+++ src/Gorilla.java
@@ -1,3 +1,25 @@
-public class Gorilla extends Mammal{
+/**
+ * Create a separate class Gorilla that can throwSomething(), eatBananas(), and climb()
+ */
+
+public class Gorilla {
+
+
+    /**
+     * For the throwSomething() method, have it print out a message indicating that the gorilla has thrown something, as well as decrease the energy level by 5
+     */
+    public void throwSomething(){}
+
+    /**
+     * For the eatBananas() method, have it print out a message indicating the gorilla's satisfaction and increase its energy by 10
+     */
+    public void eatBananas(){}
+
+    /**
+     * For the climb() method, have it print out a message indicating the gorilla has climbed a tree and decrease its energy by 10
+     */
+    public void climb(){}
+
+
 
 }
